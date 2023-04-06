4da329de1d345e69f082894041a43232ce231248
Icicvar <icicvar@racunarstvo.hr>
2023-04-07 00:34:09 +0200

    baza dela

---
+++ RestorankoAPI/Controller/TestBaza.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestorankoAPI.Models;
+
+namespace RestorankoAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestBazaController : ControllerBase
+    {
+        private readonly RestoranMenagmentContext _dbContext;
+        public TestBazaController(RestoranMenagmentContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("[action]")] 
+        public bool TestConnection()
+        {
+            return _dbContext.Database.CanConnect();
+        }   
+        [HttpGet("[action]")] 
+        public IEnumerable<Item> GetAll()
+        {
+            return _dbContext.Items;
+        }
+    }
+}
