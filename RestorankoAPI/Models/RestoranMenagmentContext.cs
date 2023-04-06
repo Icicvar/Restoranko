e0c6bb0e39970c89eae914b1185456dec0ac599a
Icicvar <icicvar@racunarstvo.hr>
2023-04-07 00:21:04 +0200

    security za cs

--- RestorankoAPI/Models/RestoranMenagmentContext.cs
+++ RestorankoAPI/Models/RestoranMenagmentContext.cs
@@ -35,7 +35,7 @@ public partial class RestoranMenagmentContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=.\\SQLExpress2;Database=RestoranMenagment;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:RestorankoCS");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
