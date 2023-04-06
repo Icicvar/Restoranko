5877eb2e91e61077aab6a76137d584371b10bdd6
Icicvar <icicvar@racunarstvo.hr>
2023-04-07 00:21:04 +0200

    security za cs

---
+++ RestorankoAPI/Controller/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestorankoAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
