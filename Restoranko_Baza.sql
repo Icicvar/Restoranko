bad003840844ed33d007432eb4b6523072450170
Marko Frančeski <62080859+francuz091@users.noreply.github.com>
2023-04-07 09:49:36 +0200

    Update Restoranko_Baza.sql

--- Restoranko_Baza.sql
+++ Restoranko_Baza.sql
@@ -1,6 +1,6 @@
 create table "User"(
 
-	IDUser int primary key,
+	IDUser int IDENTITY(1,1) primary key,
 	FirstName varchar(50) not null,
 	LastName varchar(50) not null,
 	Email varchar(100) not null,
@@ -9,23 +9,24 @@ create table "User"(
 
 )
 
+
 create table Guest(
 
-	IDGuest int primary key ,
+	IDGuest int IDENTITY(1,1) primary key ,
 	UserID int foreign key references "User"(IDUser)
 
 )
 
 create table Job(
 
-	IDJob int primary key,
+	IDJob int IDENTITY(1,1) primary key,
 	"Name" varchar(100) not null,
 
 )
 
 create table Employment(
 
-	IDEmployment int primary key,
+	IDEmployment int IDENTITY(1,1) primary key,
 	UserID int foreign key references "User"(IDUser),
 	JobID int foreign key references Job(IDJob)
 
@@ -35,13 +36,13 @@ create table Employment(
 
 create table "Table"(
 
-	IDTable int primary key,
+	IDTable int IDENTITY(1,1) primary key,
 	TableNumber int not null
 
 )
 create table "Order"(
 
-	IDOrder int primary key,
+	IDOrder int IDENTITY(1,1) primary key,
 	OrderNumber int not null,
 	OrderDate datetime not null,
 	TotalPrice int not null,
@@ -51,7 +52,7 @@ create table "Order"(
 
 create table Reservation(
 
-	IDReservation int primary key,
+	IDReservation int IDENTITY(1,1) primary key,
 	DateReservation datetime not null,
 	TableID int foreign key references "Table"(IDTable),
 	OrderID int foreign key references "Order"(IDOrder),
@@ -61,7 +62,7 @@ create table Reservation(
 
 create table Product(
 
-	IDProduct int primary key,
+	IDProduct int IDENTITY(1,1) primary key,
 	"Name" varchar(100) not null,
 	Price int not null,
 
@@ -69,7 +70,7 @@ create table Product(
 
 create table Item(
 
-	IDItem int primary key,
+	IDItem int IDENTITY(1,1) primary key,
 	OrderID int foreign key references "Order"(IDOrder),
 	ProductID int foreign key references Product(IDProduct),
 	Amount int not null,
@@ -78,4 +79,61 @@ create table Item(
 )
 
 
+
+
+
+CREATE PROCEDURE CreateUser
+    @FirstName VARCHAR(50),
+    @LastName VARCHAR(50),
+    @Email VARCHAR(100),
+    @Password VARCHAR(100)
+AS
+BEGIN
+    INSERT INTO [User] (FirstName, LastName, Email, [Password])
+    VALUES (@FirstName, @LastName, @Email, @Password)
+END
+
+
+CREATE PROCEDURE SelectUser
+    @IDUser INT
+AS
+BEGIN
+    SELECT IDUser, FirstName, LastName, Email, [Password]
+    FROM [User]
+    WHERE IDUser = @IDUser
+END
+
+CREATE PROCEDURE UpdateUser
+    @IDUser INT,
+    @FirstName VARCHAR(50),
+    @LastName VARCHAR(50),
+    @Email VARCHAR(100),
+    @Password VARCHAR(100)
+AS
+BEGIN
+    UPDATE [User]
+    SET FirstName = @FirstName, LastName = @LastName, Email = @Email, [Password] = @Password
+    WHERE IDUser = @IDUser
+END
+
+CREATE PROCEDURE DeleteUser
+    @IDUser INT
+AS
+BEGIN
+    DELETE FROM [User]
+    WHERE IDUser = @IDUser
+END
+
+CREATE PROCEDURE CheckUser
+	@Email NVARCHAR(100),
+	@Password NVARCHAR(64)
+AS
+BEGIN
+	SET NOCOUNT ON
+	IF NOT EXISTS (SELECT IDUser FROM "User" WHERE Email = @Email and Password = @Password)
+		SELECT -1
+	ELSE
+		SELECT 0
+END
+GO
 
