9d30e6544695d171504fc09eb38d8aef3567c27e
Icicvar <icicvar@racunarstvo.hr>
2023-05-03 09:23:35 +0200

    fail

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-using API.Models;
+
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<RestorankoDbContext>(options =>
-{
-    options.UseSqlServer("Name=ConnectionStrings:RestorankoConnStr");
-});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
