8fc7dbb7ddabf37cdd47db40a58a9c6074dfa126
Marko Frančeski <mfrance@racunarstvo.hr>
2023-05-16 18:01:27 +0200

    dodan repo

--- RestorankoWeb/Models/User.cs
+++ RestorankoWeb/Models/User.cs
@@ -9,23 +9,5 @@ namespace RestorankoWeb.Models
 {
     public class User
     {
-        [Display(Name = "IDUser")]
-        public int IDUser { get; set; }
-
-        [Required(ErrorMessage = "Please enter FirstName")]
-        [Display(Name = "FirstName")]
-        public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Please enter LastName")]
-        [Display(Name = "LastName")]
-        public string LastName { get; set; }
-
-        [Required]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail id is not valid")]
-        public string Email { get; set; }
-        public string Password { get; set; }
-
-        public List<User> Usersinfo { get; set; }
-
     }
 }
