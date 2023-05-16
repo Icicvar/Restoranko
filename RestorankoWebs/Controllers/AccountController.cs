4643f6a2e034dbefcd93f08530986e30397086b0
Icicvar <icicvar@racunarstvo.hr>
2023-05-16 16:44:57 +0200

    viws

---
+++ RestorankoWebs/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Security.Principal;
+
+namespace RestorankoWebs.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("~/Views/Account/Login.cshtml");
+        }
+        public IActionResult Register()
+        {
+            return View("~/Views/Account/Register.cshtml");
+}
+    }
+}
