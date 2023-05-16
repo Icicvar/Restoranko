3829085f06360c0fde96faccbb99d73a885d2619
Marko Frančeski <mfrance@racunarstvo.hr>
2023-05-16 17:43:32 +0200

    dodano

--- RestorankoWeb/Controllers/HomeController.cs
+++ RestorankoWeb/Controllers/HomeController.cs
@@ -1,30 +1,158 @@
-﻿using System;
+﻿using RestorankoWeb.Dao;
+using RestorankoWeb.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace RestorankoWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        public string value = "";
+
+        [HttpGet]
+        public ActionResult Register()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Register(User user, SqlRepository sqlRepository)
         {
-            ViewBag.Message = "Your application description page.";
+            if (Request.HttpMethod == "POST")
+            {
+                User er = new User();
+                
+                sqlRepository.CreateUser(user);
+            }
+            
+                return RedirectToAction("Welcome");
+            
+            
+        }
+        // GET: /UserLogin/  
+        public string status;
+        User m;
 
+        public ActionResult Login()
+        {
             return View();
         }
+        private readonly SqlRepository authApiClient;
 
-        public ActionResult Contact()
+        public HomeController()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            authApiClient = new SqlRepository();
         }
+        [HttpPost]
+        public async Task<ActionResult> Login(User user)
+        {
+            if (ModelState.IsValidField("email") && ModelState.IsValidField("password"))
+            {
+                bool isAuthenticated = await authApiClient.Login(user);
+
+                if (isAuthenticated)
+                {
+                    // Prijavljivanje uspješno
+                    m = user;
+                    return RedirectToAction("Welcome"); // Preusmjerite na odgovarajuću akciju i kontroler
+                }
+                else
+                {
+                    // Prijavljivanje nije uspjelo
+                    ModelState.AddModelError(string.Empty, "Neispravno korisničko ime ili lozinka");
+                }
+            }
+            m = user;
+            // Prikaz forme za prijavu s greškama
+            return View(user);
+        }
+        //[HttpPost]
+        //public ActionResult Login(User user, SqlRepository sqlRepository)
+        //{
+        //    sqlRepository.Login(user);
+
+        //    if (sqlRepository.Login(user))
+        //    {
+        //        Session["Email"] = user.Email.ToString();
+        //        return RedirectToAction("Welcome");
+        //    }
+        //    else
+        //    {
+        //        ViewData["Message"] = "User Login Details Failed!!";
+        //    }
+        //    if (user.Email.ToString() != null)
+        //    {
+        //        Session["Email"] = user.Email.ToString();
+        //        status = "1";
+        //    }
+        //    else
+        //    {
+        //        status = "3";
+        //    }
+
+
+        //    m = user;
+        //    return View();
+        //}
+
+
+            //String SqlCon = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            //SqlConnection con = new SqlConnection(SqlCon);
+            //string SqlQuery = "select Email,Password from \"User\" where Email=@Email and Password=@Password";
+            //con.Open();
+            //SqlCommand cmd = new SqlCommand(SqlQuery, con); ;
+            //cmd.Parameters.AddWithValue("@Email", user.Email);
+            //cmd.Parameters.AddWithValue("@Password", user.Password);
+            //SqlDataReader sdr = cmd.ExecuteReader();
+            //if (sdr.Read())
+            //{
+            //    Session["Email"] = user.Email.ToString();
+            //    return RedirectToAction("Welcome");
+            //}
+            //else
+            //{
+            //    ViewData["Message"] = "User Login Details Failed!!";
+            //}
+            //if (user.Email.ToString() != null)
+            //{
+            //    Session["Email"] = user.Email.ToString();
+            //    status = "1";
+            //}
+            //else
+            //{
+            //    status = "3";
+            //}
+
+            //con.Close();
+            //m=user;
+            //return View();
+            ////return new JsonResult { Data = new { status = status } };  
+        //}
+
+        [HttpGet]
+        public ActionResult Welcome()
+        {
+           
+            return View(m);
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Login", "Home");
+        }
+
+       
+
+        
     }
 }
---
+++ RestorankoWeb/Dao/SqlRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using Newtonsoft.Json;
+using RestorankoWeb.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RestorankoWeb.Dao
+{
+    public class SqlRepository : IRepository
+    {
+        private readonly HttpClient httpClient;
+
+        public SqlRepository()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:5034");
+        }
+
+        private static string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        public DataSet dataSet { get; set; }
+
+        public bool CheckUser(User user)
+        {
+            return Convert.ToInt32(SqlHelper.ExecuteScalar(cs, "checkUser", user.Email, user.Password)) == 0;
+        }
+
+        public  void CreateUser(User user)
+        {
+            SqlHelper.ExecuteNonQuery(cs, "createUser", user.FirstName, user.LastName, user.Email, user.Password);
+        }
+
+        public async Task<bool> Login(User user)
+        {
+            try
+            {
+                // Priprema podataka za prijavu
+                var loginData = new { email = user.Email, password = user.Password };
+
+                // Slanje POST zahtjeva na API endpoint za provjeru autentičnosti
+                HttpResponseMessage response = await httpClient.PostAsJsonAsync("api/Users", loginData);
+
+                // Provjera statusnog koda odgovora
+                if (response.IsSuccessStatusCode)
+                {
+                    // Prijavljivanje uspješno
+                    return true;
+                }
+                else
+                {
+                    // Prijavljivanje nije uspjelo
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Greška prilikom slanja zahtjeva ili obrade odgovora
+                Console.WriteLine("Greška prilikom prijave: " + ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
