dce1972ec59925dd807b7bc4a3616a00ef5ff49e
Marko Frančeski <mfrance@racunarstvo.hr>
2023-05-16 17:43:26 +0200

    dodan web 1

---
+++ RestorankoWeb/Dao/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestorankoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestorankoWeb.Dao
+{
+    public interface IRepository
+    {
+        bool CheckUser(User user);
+        void CreateUser(User user);
+        
+    }
+}
