5e23133945d729049c11b5d900bb2a3e3f1d34e6
Icicvar <icicvar@racunarstvo.hr>
2023-06-11 11:21:57 +0200

    ž

--- RestorankoWeb/Models/Transactionž.cs
+++ RestorankoWeb/Models/Transactionž.cs
@@ -5,7 +5,24 @@ using System.Web;
 
 namespace RestorankoWeb.Models
 {
-    public class Transactionž
+    public class Transaction
     {
+        public int Idtransaction { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public string TransactionType { get; set; } = null!;
+
+        public decimal Amount { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? UserId { get; set; }
+
+        public int? OrderId { get; set; }
+
+        public virtual Order? Order { get; set; }
+
+        public virtual User? User { get; set; }
     }
 }
